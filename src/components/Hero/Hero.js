a2ad14e78c319154deae5fade4a6aabab5e5699e
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2021-05-16 23:15:47 -0400

    lay out for slanted section done placeholders images imported sized and located

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import './../../styles/App.css'
-import 'materialize-css';
+import 'materialize-css'
+import portrait from '../elements/profile.png'
+import laptop from '../elements/laptop.png'
 
 function Hero() {
   return (
     <section className="Hero">
-      <p>Hero</p>
+      <section className="section1 ">
+        <div className="text-box">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores,
+          libero ratione illum officia  
+        </div>
+
+        <img src={laptop} id="laptop" />
+      </section>
+      <section className="section-shape">
+        <div className="hero-stripe">
+          <div className="normalize-box">
+            <div className="text-box">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores,
+              libero ratione illum officia delectus molestias ea nobis pariatur
+              nesciunt? Eius exercitationem laudantium autem necessitatibus
+              totam magni voluptate, recusandae minus quae!
+            </div>
+            <img src={portrait} id="portrait" />
+          </div>
+        </div>
+        <div className="hero-content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores,
+          libero ratione illum officia delectus molestias ea nobis pariatur
+          nesciunt? Eius exercitationem laudantium autem necessitatibus totam
+          magni voluptate, recusandae minus quae!
+        </div>
+      </section>
+      <section className="section-shape">
+        <div className="hero-stripe"></div>
+        <div className="hero-content contact-me">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores,
+          libero ratione illum officia delectus molestias ea nobis pariatur
+          nesciunt? Eius exercitationem laudantium autem necessitatibus totam
+          magni voluptate, recusandae minus quae!
+        </div>
+      </section>
     </section>
   )
 }
